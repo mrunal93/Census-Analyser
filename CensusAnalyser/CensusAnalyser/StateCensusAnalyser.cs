70fd2b87eac9d80f24cba11d5567c631a4d24f57
mrunal93 <mrn26893@gmail.com>
2020-07-29 06:51:47 +0530

    [Mrunal]Add: writeLine welcome to indian stat census and creadted NUnitTest project nad created class

---
+++ CensusAnalyser/CensusAnalyser/StateCensusAnalyser.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace CensusAnalyser
+{
+    class StateCensusAnalyser
+    {
+        
+    }
+}
