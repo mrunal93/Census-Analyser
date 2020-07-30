51c22d32c2e0dd48e0f3af0b84897248a63f4097
mrunal93 <mrn26893@gmail.com>
2020-07-30 07:31:54 +0530

    [Mrunal]Refactor: Added the test casess

--- CensusAnalyser/CensusAnalyserTest/StateCensusAnalyserTest.cs
+++ CensusAnalyser/CensusAnalyserTest/StateCensusAnalyserTest.cs
@@ -75,5 +75,74 @@ namespace CensusAnalyserTest
                 Assert.AreEqual(CensusAnalyserException.ExceptionType.HEADER_NOT_MATCH, e.type);
             }
         }
+
+        [Test]
+        public void GivenIndiaCensusCSVFileToCSVstateClass_WhenNumberOfRecordMatch_ShouldReturnTrue()
+        {
+            string INDIAN_CENSUS_CSVFILE = @"C:\Users\Admin\Documents\Census-Analyser\CensusAnalyser\CensusAnalyser\IndiaStateCensusData.csv";
+            int csvStateCensusRecords = CSVstate.GetRecord(INDIAN_CENSUS_CSVFILE);
+            int stateCensusRecords = StateCensusAnalyser.GetStateCensusRecord(INDIAN_CENSUS_CSVFILE);
+            Assert.AreEqual(stateCensusRecords, csvStateCensusRecords);
+        }
+
+        [Test]
+        public void GivenIvalidPathToCSVstateClass_WhenLoaded_ShouldReturnExceptionMessage()
+        {
+            try
+            {
+                string INDIAN_CENSUS_CSVFILE = @"C:\Users\Admin\";
+                int csvStateCensusRecords = CSVstate.GetRecord(INDIAN_CENSUS_CSVFILE);
+                int stateCensusRecords = StateCensusAnalyser.GetStateCensusRecord(INDIAN_CENSUS_CSVFILE);
+                Assert.AreEqual(csvStateCensusRecords, stateCensusRecords);
+            }
+            catch (CensusAnalyserException e)
+            {
+                Assert.AreEqual(CensusAnalyserException.ExceptionType.INVALID_FILEPATH, e.type);
+            }
+        }
+
+        [Test]
+        public void GivenIvalidFileNameToCSVstateClass_WhenLoaded_ShouldReturnExceptionMessage()
+        {
+            try
+            {
+                string INDIAN_CENSUS_CSVFILE = @"C:\Users\Admin\Documents\Census-Analyser\CensusAnalyser\CensusAnalyser\StateCensusData.csv";
+                int csvStateCensusRecords = CSVstate.GetRecord(INDIAN_CENSUS_CSVFILE);
+                int stateCensusRecords = StateCensusAnalyser.GetStateCensusRecord(INDIAN_CENSUS_CSVFILE);
+                Assert.AreEqual(csvStateCensusRecords, stateCensusRecords);
+            }
+            catch (CensusAnalyserException e)
+            {
+                Assert.AreEqual(CensusAnalyserException.ExceptionType.INVALID_FILENAME, e.type);
+            }
+        }
+
+        [Test]
+        public void GivenIvalidDelimiterToCSVstateClass_WhenLoaded_ShouldReturnExceptionMessage()
+        {
+            try
+            {
+                string INDIAN_CENSUS_CSVFILE = @"C:\Users\Admin\Documents\Census-Analyser\CensusAnalyser\CensusAnalyser\IndiaStateCensusData.csv";
+                int csvStateCensusRecords = CSVstate.GetRecord(INDIAN_CENSUS_CSVFILE);
+            }
+            catch (CensusAnalyserException e)
+            {
+                Assert.AreEqual(CensusAnalyserException.ExceptionType.DELIMITER_INCORRECT, e.type);
+            }
+        }
+
+        [Test]
+        public void GivenIvalidFileHeaderToCsvStateClass_WhenLoaded_ShouldReturnExceptionMessage()
+        {
+            try
+            {
+                string INDIAN_CENSUS_CSVFILE = @"C:\Users\Admin\Documents\Census-Analyser\CensusAnalyser\CensusAnalyser\IndiaStateCensusData.csv";
+                int csvStateCensusRecords = CSVstate.GetRecord(INDIAN_CENSUS_CSVFILE);
+            }
+            catch (CensusAnalyserException e)
+            {
+                Assert.AreEqual(CensusAnalyserException.ExceptionType.HEADER_NOT_MATCH, e.type);
+            }
+        }
     }
 }
