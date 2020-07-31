e63c1a588b37ca0302d32a79a391f32b84eebded
mrunal93 <mrn26893@gmail.com>
2020-07-31 09:59:04 +0530

    [Mrunal]Add: In this usecase coverted csv to jason using deserilisation and short the json file

---
+++ CensusAnalyser/CensusAnalyser/JsonObj.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CensusAnalyser
+{
+    class JsonObj
+    {
+        public string State { get; set; }
+        public string Population { get; set; }
+        public string AreaInSqKm { get; set; }
+        public string DensityPerSqKm { get; set; }
+    }
+}
---
+++ CensusAnalyser/CensusAnalyser/JsonStateCensus.cs
@@ -0,0 +1,49 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace CensusAnalyser
+{
+    class JsonStateCensus
+    {
+        string path;
+        public JsonStateCensus(string path)
+        {
+            this.path = path;
+        }
+        public string CsvToJSON()
+        {
+            var csv = new List<string[]>();
+            var lines = File.ReadAllLines(path);
+
+            foreach (string line in lines)
+                csv.Add(line.Split(','));
+
+            var properties = lines[0].Split(',');
+
+            var listObjResult = new List<Dictionary<string, string>>();
+
+            for (int rows = 1; rows < lines.Length; rows++)
+            {
+                var objResult = new Dictionary<string, string>();
+                for (int columns = 0; columns < properties.Length; columns++)
+                    objResult.Add(properties[columns], csv[rows][columns]);
+
+                listObjResult.Add(objResult);
+            }
+            return JsonConvert.SerializeObject(listObjResult);
+            
+        }
+
+
+        public void SortByState()
+        {
+            var listOb = JsonConvert.DeserializeObject<List<JsonObj>>(CsvToJSON());
+            var descListOb = listOb.OrderBy(x => x.State);
+            Console.WriteLine(JsonConvert.SerializeObject(descListOb));
+        }
+    }
+}
