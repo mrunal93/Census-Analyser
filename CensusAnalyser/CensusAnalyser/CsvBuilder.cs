32b5c83299b6e319a3d70d6f36fff27fc7e10cfc
mrunal93 <mrn26893@gmail.com>
2020-07-31 08:40:07 +0530

    [Mrunal]Add: REfactor of code using factory and impimentation of class

---
+++ CensusAnalyser/CensusAnalyser/CsvBuilder.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CensusAnalyser
+{
+    class CsvBuilder
+    {
+        public void CheckForException() { }
+        
+    }
+}
